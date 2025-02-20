a31d9005690806d363a5031e8865e643e7eaf7b4
Gergely Gimesi <gergely.gimesi@gmail.com>
2025-02-20 10:59:43 +0100

    allure reports and github workflow setup

--- src/test/java/tests/unit/AvailableMembersUnitTest.java
+++ src/test/java/tests/unit/AvailableMembersUnitTest.java
@@ -1,13 +1,26 @@
 package tests.unit;
 
+import io.qameta.allure.*;
+import io.qameta.allure.junit5.AllureJunit5;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import utils.ApiCallHelper;
 
 import static org.hamcrest.Matchers.*;
 
+@ExtendWith(AllureJunit5.class)
+@DisplayName("Unit - Available members")
 public class AvailableMembersUnitTest {
 
+    @Feature("Unit tests")
+    @Story("Story annotation")
+    @Tag("API")
+    @Severity(SeverityLevel.NORMAL)
     @Test
+    @DisplayName("Verify member '1'")
+    @Description("This test verifies that member 1 is available.")
     void testAvailable1Unit() {
         ApiCallHelper.get("/surveys/1/members/not-invited").then()
                 .assertThat()
@@ -16,6 +29,8 @@ public class AvailableMembersUnitTest {
     }
 
     @Test
+    @DisplayName("Verify member '50'")
+    @Description("This test verifies that member 50 is available.")
     void testAvailable50Unit() {
         ApiCallHelper.get("/surveys/1/members/not-invited").then()
                 .assertThat()
