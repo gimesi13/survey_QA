1ceef008d05a6ce97d9ca23a4a1820ffc21594b4
Gergely Gimesi <gergely.gimesi@gmail.com>
2025-02-20 15:32:20 +0100

    introducing skipped test

--- src/test/java/tests/integration/SurveyStatisticsTest.java
+++ src/test/java/tests/integration/SurveyStatisticsTest.java
@@ -1,40 +1,40 @@
-//package tests.integration;
-//
-//import org.junit.jupiter.api.BeforeAll;
-//import org.junit.jupiter.api.Test;
-//import api.SurveyStatApi;
-//
-//class SurveyStatisticsTest {
-//
-//    private static SurveyStatApi surveyStatApi;
-//
-//    @BeforeAll
-//    static void setup() {
-//        surveyStatApi = new SurveyStatApi();
-//    }
-//
-//    @Test
-//    void testSurveyIds() {
-//        surveyStatApi.testSurveyStatistic("Survey Id", "surveyId");
-//    }
-//
-//    @Test
-//    void testNumberOfCompletes() {
-//        surveyStatApi.testSurveyStatistic("Number of Completes", "numberOfCompletes");
-//    }
-//
-//    @Test
-//    void testNumberOfFiltered() {
-//        surveyStatApi.testSurveyStatistic("Number of Filtered", "numberOfFiltered");
-//    }
-//
-//    @Test
-//    void testNumberOfRejected() {
-//        surveyStatApi.testSurveyStatistic("Number of Rejected", "numberOfRejected");
-//    }
-//
-//    @Test
-//    void testAverageLength() {
-//        surveyStatApi.testSurveyStatistic("Average Length", "avgLengthOfSurvey");
-//    }
-//}
+package tests.integration;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import api.SurveyStatApi;
+
+class SurveyStatisticsTest {
+
+    private static SurveyStatApi surveyStatApi;
+
+    @BeforeAll
+    static void setup() {
+        surveyStatApi = new SurveyStatApi();
+    }
+
+    @Test
+    void testSurveyIds() {
+        surveyStatApi.testSurveyStatistic("Survey Id", "surveyId");
+    }
+
+    @Test
+    void testNumberOfCompletes() {
+        surveyStatApi.testSurveyStatistic("Number of Completes", "numberOfCompletes");
+    }
+
+    @Test
+    void testNumberOfFiltered() {
+        surveyStatApi.testSurveyStatistic("Number of Filtered", "numberOfFiltered");
+    }
+
+    @Test
+    void testNumberOfRejected() {
+        surveyStatApi.testSurveyStatistic("Number of Rejected", "numberOfRejected");
+    }
+
+    @Test
+    void testAverageLength() {
+        surveyStatApi.testSurveyStatistic("Average Length", "avgLengthOfSurvey");
+    }
+}
