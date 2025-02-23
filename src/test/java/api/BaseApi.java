4df239cb3f8c6f1d03ad69997bf8e0972f48eb51
Gergely Gimesi <gergely.gimesi@gmail.com>
2025-02-23 14:07:40 +0100

    test logs

--- src/test/java/api/BaseApi.java
+++ src/test/java/api/BaseApi.java
@@ -1,13 +1,23 @@
 package api;
 
+import io.qameta.allure.Allure;
+import io.restassured.response.Response;
 import utils.ApiCallHelper;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
 public class BaseApi {
 
     public void statusTest(String endpoint, int expectedStatus) {
-        System.out.println("endpoint: " + endpoint + ", expectedStatus: " + expectedStatus);
-        ApiCallHelper.get(endpoint)
-                .then()
-                .statusCode(expectedStatus);
+        Allure.step("Sending API request to: " + endpoint);
+        Response response = ApiCallHelper.get(endpoint);
+
+        try {
+            assertThat("Status code mismatch for: " + endpoint, response.statusCode(), equalTo(expectedStatus));
+        } catch (AssertionError e) {
+            Allure.addAttachment("API Failure - " + endpoint, "text/plain", response.body().asPrettyString());
+            throw e;
+        }
     }
 }
--- src/test/java/tests/unit/SurveyStatisticsUnitTest.java
+++ src/test/java/tests/unit/SurveyStatisticsUnitTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import utils.ApiCallHelper;
 
+import io.restassured.response.Response;
 import static org.hamcrest.Matchers.*;
 
 @ExtendWith(AllureJunit5.class)
@@ -19,27 +20,45 @@ public class SurveyStatisticsUnitTest {
     @DisplayName("Verify Statistics for Survey 1")
     @Description("Verifies the statistics data for survey with ID 1")
     void testSurvey1Unit() {
-        ApiCallHelper.get("/surveys/statistics").then()
-                .assertThat()
-                .body("surveyName", hasItem("Survey 01"))
-                .body("[0].surveyId", equalTo(1))
-                .body("[0].numberOfCompletes", equalTo(21))
-                .body("[0].numberOfFiltered", equalTo(8))
-                .body("[0].numberOfRejected", equalTo(4))
-                .body("[0].avgLengthOfSurvey", equalTo(13));
+        Allure.step("Sending API request to fetch survey statistics");
+        Response response = ApiCallHelper.get("/surveys/statistics");
+
+        try {
+            Allure.step("Validating response for Survey 1");
+            response.then()
+                    .assertThat()
+                    .body("surveyName", hasItem("Survey 01"))
+                    .body("[0].surveyId", equalTo(1))
+                    .body("[0].numberOfCompletes", equalTo(21))
+                    .body("[0].numberOfFiltered", equalTo(8))
+                    .body("[0].numberOfRejected", equalTo(4))
+                    .body("[0].avgLengthOfSurvey", equalTo(13));
+        } catch (AssertionError e) {
+            Allure.addAttachment("Survey 1 API Response", "text/plain", response.body().asPrettyString());
+            throw e;
+        }
     }
 
     @Test
     @DisplayName("Verify Statistics for Survey 100")
     @Description("Verifies the statistics data for survey with ID 100")
     void testSurvey100Unit() {
-        ApiCallHelper.get("/surveys/statistics").then()
-                .assertThat()
-                .body("surveyName", hasItem("Survey 100"))
-                .body("[99].surveyId", equalTo(100))
-                .body("[99].numberOfCompletes", equalTo(24))
-                .body("[99].numberOfFiltered", equalTo(3))
-                .body("[99].numberOfRejected", equalTo(5))
-                .body("[99].avgLengthOfSurvey", equalTo(15));
+        Allure.step("Sending API request to fetch survey statistics");
+        Response response = ApiCallHelper.get("/surveys/statistics");
+
+        try {
+            Allure.step("Validating response for Survey 100");
+            response.then()
+                    .assertThat()
+                    .body("surveyName", hasItem("Survey 100"))
+                    .body("[99].surveyId", equalTo(100))
+                    .body("[99].numberOfCompletes", equalTo(24))
+                    .body("[99].numberOfFiltered", equalTo(3))
+                    .body("[99].numberOfRejected", equalTo(5))
+                    .body("[99].avgLengthOfSurvey", equalTo(15));
+        } catch (AssertionError e) {
+            Allure.addAttachment("Survey 100 API Response", "text/plain", response.body().asPrettyString());
+            throw e;
+        }
     }
 }
